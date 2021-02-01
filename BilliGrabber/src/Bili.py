53e2d4f27216328dc92eb9c55265a05dcfd4fe71
robinsonweng <robinsonweng100@gmail.com>
2021-02-01 23:38:01 +0900

    check if url is valid

--- BilliGrabber/src/Bili.py
+++ BilliGrabber/src/Bili.py
@@ -7,6 +7,20 @@ class Analyzer(object):
         self.url = url
 
     def check_if_valid(self):
+        """
+        check if url is valid\n
+        """
+        url_head = 'https://www.bilibili.com/video/'
+        if not self.url.startswith(url_head):
+            self.url = f'{url_head}{self.url}'
+
         r = requests.get(self.url)
-        print(r)
-        print(r.text)
+        r.raise_for_status()
+
+
+if __name__ == "__main__":
+    singlevid_url = "https://www.bilibili.com/video/BV1kW411q7N1"
+    vidfolder_url = "https://www.bilibili.com/video/BV1AW411M"
+    video_id = "BV1AW411M"
+    a = Analyzer(singlevid_url)
+    a.check_if_valid()
